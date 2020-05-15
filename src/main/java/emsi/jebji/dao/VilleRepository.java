9fdb653a462f9e146efb0d64bf033593610f48a0
Ayoub JEBJI <jebji.ayoub@emsi-edu.ma>
2020-05-15 19:53:30 +0000

    Upload

---
+++ src/main/java/emsi/jebji/dao/VilleRepository.java
@@ -0,0 +1,12 @@
+package emsi.jebji.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import emsi.jebji.entities.Ville;
+
+@RepositoryRestResource
+@CrossOrigin("*")
+public interface VilleRepository extends JpaRepository<Ville, Long> {
+}
